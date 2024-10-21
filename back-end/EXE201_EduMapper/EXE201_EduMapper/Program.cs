f46696d30330f913c8f0af712343c20a5d8ad1c8
Nam Ng Phuong <npnam03@gmail.com>
2024-10-21 18:04:45 +0700

    [Fix]_CORS_BE

--- back-end/EXE201_EduMapper/EXE201_EduMapper/Program.cs
+++ back-end/EXE201_EduMapper/EXE201_EduMapper/Program.cs
@@ -77,7 +77,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigins",
         builder =>
         {
-            builder.WithOrigins("http://103.177.111.32:7010/swagger/index.html")
+            builder.WithOrigins("http://localhost:5173/")
                    .AllowAnyHeader()
                    .AllowAnyMethod()
                    .AllowCredentials();
